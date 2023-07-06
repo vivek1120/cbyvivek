dc586a131d91ea5b947b04ac14174aadb5a49843
vivek1120 <vivekp95194@gmail.com>
2023-07-06 11:39:12 +0530

    anatomy of the c proggram

---
+++ anatomy.c
@@ -0,0 +1,11 @@
+#include<stdio.h> // header file * header files are contains the functions like printf,scanf
+int main() //main function * satarting of the program 
+{
+    int a; //declare the datatype and variable 
+    a = 39;  //assign the value for variable 
+    printf("print the integer: %d",a);// printf is a function show somthing in display
+    return 0; //return 0 means everything went well  
+              // return means other programmer or progaram and system know the result of the program,
+              // the return shows the everything went well or not 
+     
+}
