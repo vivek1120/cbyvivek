bee7774856135aef1962a915d6b4d0d1366b29a5
vivek1120 <vivekp95194@gmail.com>
2023-07-06 11:40:03 +0530

    learning about functions

---
+++ function.c
@@ -0,0 +1,17 @@
+/*there are two ways to declare  a function
+        *with decleration 
+        *without decleration
+
+*/ 
+#include<stdio.h>
+int add(int a,int b); // this is called function prototype
+       
+int main()
+{
+        printf("%d",add(6,7));
+}
+int add(int a,int b){
+        int c= a + b;
+        return c;
+
+}
