d912b3a2b1ecab7d5a7bf4376e047203044810be
vivek1120 <vivekp95194@gmail.com>
2023-07-14 08:20:13 +0530

    playing with character datatype

--- characters.c
+++ characters.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-int main() 
-{
-char ch,s[8], sen[15];
-scanf("%c\n%s\n%[^\n]s", &ch,s,sen);
-printf("%c\n%s\n%s", ch,s,sen);
+
+int main() {
+  char ch, s[8], sen[15];
+  scanf("%c\n%s\n%15[^\n]s", &ch, s, sen);
+  printf("%c\n%s\n%s", ch, s, sen);
+  return 0;
 }
