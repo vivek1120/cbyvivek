f707c5ad9244c5f34217f6ee58c147fcb1c05881
vivek1120 <vivekp95194@gmail.com>
2023-07-14 08:24:18 +0530

    basic dynamic input values and output

--- dynamicinput.c
+++ dynamicinput.c
@@ -3,24 +3,28 @@ int main()
 {
     int x;
     float y;
-    char az;
+    char z;
+
     printf("enter a char value: ");
-    scanf("%c",&az);
+    scanf("%c",&z);
+
     printf("enter a int value: ");
     scanf("%d",&x);
+
     printf("enter a float value: ");
     scanf("%f",&y);
     
     
     printf("given value is int %d\n",x);
     printf(" given value is float %f\n",y);
-    printf(" given value is char %c\n",az);
+    printf(" given value is char %c\n",z);
 
-
+// this is another code
     int a;
     char b;
     float c;
     printf("enter the values: ");
     scanf("%d%c%f", &a,&b,&c);
+    
     printf("int= %d \nchar = %c \nfloat = %.1f\n",a,b,c);
 }
