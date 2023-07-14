ffc1830accce2b99fb052b654cfc5621c6b3060b
vivek1120 <vivekp95194@gmail.com>
2023-07-14 08:17:57 +0530

    learning arithmetic operation

--- arithmetic.c
+++ arithmetic.c
@@ -3,23 +3,16 @@
         Addition:   +
         Subtraction: -
         Multiplication: *
-        Division: /
+        Division:/ (quotiont of division)
         Modulo: %  (reminder of division)*/
 #include<stdio.h>
 int main ()
 {
     int a=5,b=10;
-    printf("sum of : %d\n",a+b);
+    printf("addition of : %d\n",a+b);
     printf("difference of  : %d\n",a-b);
     printf("multiplication of : %d\n",a*b);
     printf("quotiont of  : %d\n",a/b);
-    printf("reminder of  : %d\n",a%b);//int is called modulo operater
-    
-
-
-
-    int a,b;
-    printf("enter a number: ");
-    scanf("%d%d",&a,&b);
-    printf("%d", a+b,a-b);
+    printf("reminder of  : %d\n",a%b);
+    return 0;
 }
