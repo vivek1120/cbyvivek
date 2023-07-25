c76b644f5086532a23dc3c6ecdd6b4dcc7e00de0
vivek1120 <vivekp95194@gmail.com>
2023-07-25 15:16:43 +0530

    this program is tell about varaible life cycle or life span of variable

--- scope.c
+++ scope.c
@@ -11,26 +11,26 @@ int main()
     {
         printf("for loop :%d\n",addititon());
     }
-    // static int x = 8;
-    // printf("value of h global variable:%d\n",h);
-    // printf("value of x static variable:%d\n",x);
+    static int x = 8;
+    printf("value of h global variable:%d\n",h);
+    printf("value of x static variable:%d\n",x);
 
-    // printf("address of if global variable:%p\n",&h);
-    // printf("address of static variable   :%p\n",&x);
+    printf("address of if global variable:%p\n",&h);
+    printf("address of static variable   :%p\n",&x);
 
-    // printf("PID : %d\n",getpid());
-    // printf("function value of v: %d\n",addititon());
+    printf("PID : %d\n",getpid());
+    printf("function value of v: %d\n",addititon());
 
 
-    // if (1)
-    // {
-    //     int a = 9;
-    //     printf("if value  a:%d\n",a);
-    //     printf("if address a:%p\n",&a);
+    if (1)
+    {
+        int a = 9;
+        printf("if value  a:%d\n",a);
+        printf("if address a:%p\n",&a);
 
         
-    //     printf("value of static inside function :%d\n",x);
-    // }
+        printf("value of static inside function :%d\n",x);
+    }
     
     return 0;
 }
@@ -44,9 +44,9 @@ int addititon()
     v++;
     printf("value of v :%d\n",v);
     printf("address of v:%p\n",&v);
-    // int a = 6;
-    // printf("value of function a :%d\n",a);
-    // printf("address of function a:%p\n",&a);
+    int a = 6;
+    printf("value of function a :%d\n",a);
+    printf("address of function a:%p\n",&a);
 
 
 
